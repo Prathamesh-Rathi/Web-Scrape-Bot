240d7dd36c84df5773b6373542a4f9eb86084478
Prathamesh-Rathi <prathameshrathi78@gmail.com>
2022-02-21 13:58:47 +0530

    Automatic Mailing

---
+++ Sendmail.py
@@ -0,0 +1,40 @@
+
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+def send(filename):
+    from_id = 'prathameshrathi78@gmail.com'
+    to_id = 'sunitarathi0909@gmail.com'
+    subject = "Finance Report"
+
+    msg = MIMEMultipart()
+    msg['From'] = from_id
+    msg['To'] = to_id
+    msg['Subject'] = subject
+
+    body = "<b>Today's Stock Market Report</b>"
+    msg.attach(MIMEText( body , 'html'))
+
+    my_file  = open(filename, 'rb') 
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((my_file).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename= ' + filename)
+    msg.attach(part)
+
+    message = msg.as_string()
+
+
+    server = smtplib.SMTP('smtp.gmail.com' , 587)
+    #encription to make server strong
+    server.starttls()
+    server.login(from_id , 'moggbatljwjwziop')
+
+    server.sendmail(from_id, to_id , message)
+
+    server.quit()
